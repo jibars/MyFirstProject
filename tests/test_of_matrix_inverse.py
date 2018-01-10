f00e330cb3bb7316211c799e07ad908b221ffb45
Juan Antonio <ja.ibars@gmail.com>
2018-01-10 16:23:16 +0100

    change name of function

--- tests/test_of_matrix_inverse.py
+++ tests/test_of_matrix_inverse.py
@@ -15,7 +15,7 @@ class BasicTests(unittest.TestCase):
         b = [[1.0, 0.0, 1.0], [1.0, 2.0, 1.0], [1.0, 1.0, 0.0]]
         ab = [[3.0, 1.0, 2.0], [3.0, 0.0, 3.0], [7.0, 3.0, 6.0]]
 
-        self.assertEqual(ab, math.matrix_product(a, b))
+        self.assertEqual(ab, math.product(a, b))
 
     def test_determinant_is_zero(self):
         a = [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]]
